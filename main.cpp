f312765e31f7d2e22cdf4ab626bfceca88af7c3d
T1n9 <futinn@gmail.com>
2016-09-09 15:41:43 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,15 +10,9 @@ int main(int argc, char *argv[])
     }msgToSend;
 
     msgToSend.canId = 0;
-    msgToSend.bytes[0] = 1;
-    msgToSend.bytes[1] = 2;
-    msgToSend.bytes[2] = 3;
-    msgToSend.bytes[3] = 4;
-    msgToSend.bytes[4] = 5;
-    msgToSend.bytes[5] = 6;
-    msgToSend.bytes[6] = 7;
-    msgToSend.bytes[7] = 8;
-
+    for(int i=0;i<8;i++)
+    msgToSend.bytes[i] = i;
+    
     std::string deviceName("can0");
     sendDriver _driver(deviceName);
     QString res = _driver.sendByte(msgToSend.canId,msgToSend.bytes);
